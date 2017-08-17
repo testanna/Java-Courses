20dd53ec640c6fd35b22c7976ad5a51184ca48c8
Khitrova Anna <akhitrova@lanit.ru>
2017-08-17 18:53:40 +0300

    Рефакторинг по заданиям

--- sandbox/src/test/java/stqa/sandbox/PointTests.java
+++ sandbox/src/test/java/stqa/sandbox/PointTests.java
@@ -12,20 +12,20 @@ public class PointTests {
     public void distanceWithSameXValueTest() {
         Point p1 = new Point(1, 2);
         Point p2 = new Point( 3, 2);
-        Assert.assertEquals(2.0, p1.distance(p2));
+        Assert.assertEquals(p1.distance(p2), 2.0);
     }
 
     @Test
     public void distanceWithSameYValueTest() {
         Point p1 = new Point(4, 2);
         Point p2 = new Point( 4, 10);
-        Assert.assertEquals(8.0, p1.distance(p2));
+        Assert.assertEquals(p1.distance(p2), 8.0);
     }
 
     @Test
     public void distanceWithNegativesValuesTest() {
         Point p1 = new Point(-3, -2);
         Point p2 = new Point( -3, -5);
-        Assert.assertEquals(3.0, p1.distance(p2));
+        Assert.assertEquals(p1.distance(p2), 3.0);
     }
 }
