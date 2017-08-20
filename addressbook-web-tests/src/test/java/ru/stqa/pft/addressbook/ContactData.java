160c333914ea5934d7f085e394508ec0d6158aad
Khitrova Anna <akhitrova@lanit.ru>
2017-08-20 15:33:35 +0300

    Добавление теста на создание контакта

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+    private final String firstName;
+    private final String lastName;
+    private final String address;
+    private final String homeTelephone;
+    private final String email;
+
+    public ContactData(String firstName, String lastName, String address, String homeTelephone, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.homeTelephone = homeTelephone;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getHomeTelephone() {
+        return homeTelephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
