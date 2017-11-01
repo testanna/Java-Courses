685bf83135fc9685ccc0dca2f819ea768d2b2f60
Khitrova Anna <akhitrova@lanit.ru>
2017-11-01 15:08:11 +0300

    исправление версий

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -63,7 +64,7 @@ public class ApplicationManager {
 
             DesiredCapabilities capabilities = new DesiredCapabilities();
             capabilities.setBrowserName(browser);
-            capabilities.setPlatform(org.openqa.selenium.Platform.WINDOWS);
+            capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win8.1")));
             wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
         }
 
