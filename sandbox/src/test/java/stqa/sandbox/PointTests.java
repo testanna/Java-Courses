d34763d9c11b4ff06fb1f2b8b3a368b862dc159a
Khitrova Anna <akhitrova@lanit.ru>
2017-11-01 12:27:27 +0300

    1

--- sandbox/src/test/java/stqa/sandbox/PointTests.java
+++ sandbox/src/test/java/stqa/sandbox/PointTests.java
@@ -9,7 +9,8 @@ public class PointTests {
     public void distanceWithSameXValueTest() {
         Point p1 = new Point(1, 2);
         Point p2 = new Point( 3, 2);
-        Assert.assertEquals(p1.distance(p2), 2.0);
+        //Assert.assertEquals(p1.distance(p2), 2.0);
+        Assert.assertEquals(p1.distance(p2), 3.0);
     }
 
     @Test
