6d6edb6e4fb7a529ef7545392d9259f18610d0e4
Khitrova Anna <akhitrova@lanit.ru>
2017-11-03 14:31:44 +0300

    задержка перед логином

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -14,7 +14,8 @@ public class SessionHelper extends HelperBase {
         super(wd);
     }
 
-    public void login(String username, String password) {
+    public void login(String username, String password) throws InterruptedException {
+        Thread.sleep(10000);
         type(By.name("user"), username);
         type(By.name("pass"), password);
         click(By.xpath("//form[@id='LoginForm']/input[3]"));
