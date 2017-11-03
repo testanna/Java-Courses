beee4e7c80daf10eeac3f7f2a218647676117352
Khitrova Anna <akhitrova@lanit.ru>
2017-11-03 14:40:54 +0300

    Убрали ожидание перед логином

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -15,7 +15,6 @@ public class SessionHelper extends HelperBase {
     }
 
     public void login(String username, String password) throws InterruptedException {
-        Thread.sleep(10000);
         type(By.name("user"), username);
         type(By.name("pass"), password);
         click(By.xpath("//form[@id='LoginForm']/input[3]"));
